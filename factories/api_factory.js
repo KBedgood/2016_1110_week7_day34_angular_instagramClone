1d6a5da83e9203f3c909424a73d54e43599c7363
Kristie A Bedgood <kristieabedgood@Kristies-MacBook-Pro.local>
2016-11-11 00:17:34 -0500

    end of thursday. stopping point.

---
+++ factories/api_factory.js
@@ -0,0 +1,82 @@
+/*----------------------------------------------------------------------------------------------------------------------------------------------------
+AngularJS 1.X
+A module contains the different components of an AngularJS app
+A controller manages the app's data
+An expression displays values on the page
+A filter formats the value of an expression
+------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------
+The FACTORY manipulates data (adding / deleting to array, marking as complete...)
+------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+(function() {
+          'use strict';
+
+          angular
+               .module('kristieglam')
+               .factory('API', function($http) {
+
+                         // Get Images:
+                         // Type: GET
+                         // URL: http://instagramcloneclass.herokuapp.com/images
+                         // Header: X_CSRF_TOKEN
+                         const getPhotos = function() {
+                              var promise = $http({
+                                   method: 'GET',
+                                   Headers: { 'X_CSRF_TOKEN': 'kristie' },
+                                   url: 'http://instagramcloneclass.herokuapp.com/images'
+                              });
+                              return promise;
+                         }
+
+                         // Get Single Image:
+                         // Type: GET
+                         // URL: http: //instagramcloneclass.herokuapp.com/images/IMAGE_ID_HERE
+                         // Header: X_CSRF_TOKEN
+                         const getSingleImage = function(id) {
+                              var call = $http({
+                                   method: 'GET',
+                                   headers: { 'X_CSRF_TOKEN': 'kristie' },
+                                   url: `http://instagramcloneclass.herokuapp.com/images/${{id}}`,
+                              });
+                              return promise;
+                         }
+
+                         // Creating an image:
+                         // Type: POST
+                         // URL : http://instagramcloneclass.herokuapp.com/image/post
+                         // Data: title,description,url 
+                         // Header: X_CSRF_TOKEN
+                         const postImage = function(title, description, url) {
+                              var call = $http({
+                                   method: 'POST',
+                                   headers: { 'X_CSRF_TOKEN': 'kristie' },
+                                   url: 'http://instagramcloneclass.herokuapp.com/image/post',
+                              });
+                              return promise;
+                         }
+
+                         // Like Image:
+                         // Type: POST
+                         // URL: http://instagramcloneclass.herokuapp.com/images/vote
+                         // Data: imageid
+                         const likeImage = function(imageid) {
+                              var call = $http({
+                                   method: 'POST',
+                                   headers: { 'X_CSRF_TOKEN': 'kristie' },
+                                   url: 'http://instagramcloneclass.herokuapp.com/images/vote',
+                              });
+                              return promise;
+
+
+
+                              return {
+                                   getPhotos,
+                                   getSingleImage,
+                                   postImage,
+                                   likeImage
+                              }
+                         });
+
+               })();
